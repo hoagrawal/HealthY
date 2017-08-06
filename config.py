f908cb33fffff59b100499921015743341d8e46d
Hari Agrawal <hariom.wt@gmail.com>
2017-08-07 00:11:47 +0530

    reformat py files

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 def read_config(key):
+    # Read property key from property file
 
-#Read property key from property file
-
-	health_prop_file=open("healthY.properties", 'r')
-	for line in health_prop_file:
-		if key in line:
-			keyval=line.rstrip().split("=")[1]
-	return keyval
+    health_prop_file = open("healthY.properties", 'r')
+    for line in health_prop_file:
+        if key in line:
+            keyval = line.rstrip().split("=")[1]
+    return keyval
--- heartrate.py
+++ heartrate.py
@@ -5,16 +5,17 @@ import requests
 import config
 
 
-def heartrate (user_id, period):
-	''' (string,strig) -> string
+def heartrate(user_id, period):
+    # type: (object, object) -> object
+    ''' (string,strig) -> string
 	Fetches heart rate of given user id from fitbit api
 	'''
-	head={"Authorization":"Bearer "+config.read_config("FITBIT_OAUTH_TOKEN")}
-	api_base="https://api.fitbit.com/1/user/"
-	api="/activities/heart/date/today/"
-	get_request=(api_base+user_id+api+period+"d.json")
-	r=requests.get(get_request, headers=head)
-	resp=json.loads(r.content)
-	print (json.dumps(resp))
+    head = {"Authorization": "Bearer " + config.read_config("FITBIT_OAUTH_TOKEN")}
+    api_base = "https://api.fitbit.com/1/user/"
+    api = "/activities/heart/date/today/"
+    get_request = (api_base + user_id + api + period + "d.json")
+    r = requests.get(get_request, headers=head)
+    resp = json.loads(r.content)
+    print(json.dumps(resp))
 
 heartrate("5T33PH","7")
