4d81ac4aee3b9e86aab8d96d979bad6832850c63
Hari Agrawal <hariom.wt@gmail.com>
2017-08-06 20:00:26 +0530

    refactoring of config file

---
+++ config.py
@@ -0,0 +1,9 @@
+def read_config(key):
+
+#Read property key from property file
+
+	health_prop_file=open("healthY.properties", 'r')
+	for line in health_prop_file:
+		if key in line:
+			keyval=line.rstrip().split("=")[1]
+	return keyval
--- heartrate.py
+++ heartrate.py
@@ -2,23 +2,14 @@
 from __future__ import print_function
 import json
 import requests
-
-def read_config(key):
-
-#Read FITBIT_OAUTH_TOKEN from property file
-
-	health_prop_file=open("healthY.properties", 'r')
-	for line in health_prop_file:
-		if key in line:
-			keyval=line.rstrip().split("=")[1]
-	return keyval
+import config
 
 
 def heartrate (user_id, period):
 	''' (string,strig) -> string
 	Fetches heart rate of given user id from fitbit api
 	'''
-	head={"Authorization":"Bearer "+read_config("FITBIT_OAUTH_TOKEN")}
+	head={"Authorization":"Bearer "+config.read_config("FITBIT_OAUTH_TOKEN")}
 	api_base="https://api.fitbit.com/1/user/"
 	api="/activities/heart/date/today/"
 	get_request=(api_base+user_id+api+period+"d.json")
